18376e256a883ff6ff2d4de9bfe66291c8e31897
Rich Ting (丁立淇) <A2433@unimicron.com>
2022-03-09 16:51:15 +0800

    add concat three table fuction

--- utils/merge_data.py
+++ utils/merge_data.py
@@ -8,6 +8,7 @@ Created on Tue Mar  8 14:45:31 2022
 import pandas as pd
 import os
 from tqdm import tqdm
+from datetime import timedelta
 
 BASE_DIR = os.path.abspath(os.path.join('..','data'))
 TEST_SAVE = os.path.join(BASE_DIR,'test.csv')
@@ -37,8 +38,14 @@ def fqc_data(fqc_path):
         result.append(df)
     result = pd.concat(result)
     result.columns = ['FQC_date','Part_No','Date_Code','lot','Total_Strips','OK']
+    result['Part_No'] = result['Part_No'].str[:9]
+    result = result.replace(' ', '', regex=True)
+    #result['lot']=result['lot'].str[:4]
+    result.lot = pd.to_numeric(result.lot, errors='coerce')
+    result.Date_Code = pd.to_numeric(result.Date_Code, errors='coerce')
     result_group = result.groupby(['Part_No','Date_Code','lot']).agg({'FQC_date':'last','Total_Strips':'sum','OK':'sum'}).reset_index()
-    return result,result_group
+    result_group['f_repeat'] = result_group.index
+    return result_group
 
 
 def oqc_data(oqc_path):
@@ -54,19 +61,55 @@ def oqc_data(oqc_path):
     result['OQC_date'] = result['OQC_date'].astype(str).str[:10]
     result['OQC_date'] = pd.to_datetime(result['OQC_date'])
     result['lot'] = result['lot'].astype(str)
-    result = result[result['lot'].str.contains('V')]
+    result['OQC_AVI'] = 0
+    result.loc[result['lot'].str.contains('V'),'OQC_AVI'] = 1
     result['lot']=result['lot'].str[:4]
     result['Part_No'] = result['Part_No'].str[:9]
-    result.check_times = 1
-    result.loc[result.reject_times == '退回'] = 1
-    result.loc[result.reject_times != '退回'] = 0
-    print(len(result))
-    result_group = result.groupby(['Part_No','Date_Code','lot']).agg({'OQC_date':'last','check_times':'sum','reject_times':'sum'}).reset_index()
-    print(len(result_group))
-    return result,result_group
-    
+    result.lot = pd.to_numeric(result.lot, errors='coerce')
+    result.Date_Code = pd.to_numeric(result.Date_Code, errors='coerce')
+    result.loc[result.reject_times != '退回','reject_times'] = 0
+    result.loc[result.reject_times == '退回','reject_times'] = 1
+    result_group = result.groupby(['Part_No','Date_Code','lot']).agg({'OQC_date':'last','check_times':'count','reject_times':'sum','OQC_AVI':'max'}).reset_index()
+    result_group['o_repeat'] = result_group.index
+    return result_group
 
+
+def ai_data(ai_path):
+    print('STATUS: arrange ai data')
+    df = pd.read_csv(ai_path)
+    df = df[['AVI','VRS','Part_No','lot','strips','CheckTime(min)','OK','NG','ALL','type','size','AI']]
+    df['Part_No'] = df.Part_No.str[:9]
+    df.lot = pd.to_numeric(df.lot, errors='coerce')
+    df = df[df.AI == 'ON']
+    df = df.drop('AI',axis = 1).reset_index()
+    df['AVI'] = pd.to_datetime(df['AVI'], format='%Y%m%d')
+    df = df.groupby(['Part_No','lot','size']).resample('14D', on='AVI').agg({'VRS':'last','strips':'sum','CheckTime(min)':'sum','OK':'sum','NG':'sum','ALL':'sum','type':'last'}).reset_index()
+    df['repeat_check'] = df.index
+    df = df.dropna()
+    result = []
+    for i in range(5):
+        df_t = df.copy()
+        df_t['Date_Code'] = (df_t['AVI'] - timedelta(days=7*abs(i))).dt.strftime('%U%y')
+        result.append(df_t)
+    result = pd.concat(result)
+    result.lot = pd.to_numeric(result.lot, errors='coerce')
+    result.Date_Code = pd.to_numeric(result.Date_Code, errors='coerce')
+    return result
+    
+def all_concat(ai_path,fqc_path,oqc_path):
+    ai_df = ai_data(ai_path)
+    fqc_df = fqc_data(fqc_path)
+    oqc_df = oqc_data(oqc_path)
+    con_df = ai_df.merge(fqc_df.merge(oqc_df,on=['Part_No','lot','Date_Code'],how='left'),on=['Part_No','lot','Date_Code'],how='left')
+    result = []
+    for i in range(con_df.repeat_check.max()+1):
+        row = con_df[con_df.repeat_check == i]
+        row = row.sort_values(by=['OQC_date','FQC_date','VRS'],ascending=False)
+        row = row.head(1)
+        result.append(row)
+    result = pd.concat(result)
+    result.to_csv(TEST_SAVE)
 
 if __name__ == '__main__':
-    a,b = oqc_data(OQC_DIR)
-    b.to_csv(TEST_SAVE)
+    all_concat(AI_DIR,FQC_DIR,OQC_DIR)
+    #ai_data(AI_DIR)
