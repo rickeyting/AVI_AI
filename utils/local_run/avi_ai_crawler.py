4caf2fdd11f66cfcc1ad7d35f9d9b9dc022b9bfe
Rich Ting (丁立淇) <A2433@unimicron.com>
2022-03-16 14:12:22 +0800

    replace location of getting 2d code

--- utils/local_run/avi_ai_crawler.py
+++ utils/local_run/avi_ai_crawler.py
@@ -47,7 +47,7 @@ def check_unprocessed_date(check_dir,ai_df=ai_df):
     last_date = '20210101'
     if len(ai_df) != 0:
         last_date = ai_df.iloc[-1]['AVI']
-        last_date = (datetime.strptime(str(last_date), '%Y%m%d')-timedelta(days=1)).strftime('%Y%m%d')
+        last_date = (datetime.strptime(str(last_date), '%Y%m%d')-timedelta(days=5)).strftime('%Y%m%d')
     print('UPDATE FROM ' +last_date)
     unprocessed_list = [i for i in present_list if (len(i)==8)&(i>=last_date)]
     #print('STATUS:getting unprocessed date Done')
@@ -103,19 +103,19 @@ def get_lot_info(lot_path,ai_df):
             VRS = datetime.fromtimestamp(np.median(vrs_time)).strftime("%Y-%m-%d") 
             strips = len(vrs_time)
             try:
-                Date_code = concat_df['Step_Code'].max()
+                Date_code = concat_df.sort_values(by = 'Step_Code').iloc[0,2]
+            except:
+                Date_code = np.nan
+            try:
                 OK_m = len(concat_df[concat_df.AI_Flag.astype(str) == 'OK'])
                 NG_m = len(concat_df[concat_df.AI_Flag.astype(str) != 'OK'])
                 if OK_m == 0:
                     AI = 'Unactivated'
                 else:
                     AI = 'ON'
-                if ai_status == 'unfiltered':
-                    AI = 'unfiltered'
                 ALL_m = OK_m + NG_m
                 filter_rate = round(OK_m/ALL_m,2) 
             except:
-                Date_code = np.nan
                 OK_m = np.nan
                 NG_m = np.nan
                 ALL_m = np.nan
@@ -150,7 +150,8 @@ def get_lot_info(lot_path,ai_df):
                 size = np.nan
                 model = np.nan
                 Type = np.nan
-                
+            if ai_status == 'unfiltered':
+                AI = 'unfiltered'
             concat_list.append([AVI,part,Date_code,VRS,Part_No,tape_lot,vrs_id,strips,CheckTime,OK_m,NG_m,ALL_m,filter_rate,visper,AI,size,Type,model])
     if len(concat_list) > 0:
         add_df = pd.DataFrame(np.array(concat_list),columns=['AVI','part','Date_Code','VRS','Part_No','lot','vrs_id','strips','CheckTime(min)','OK','NG','ALL','filter rate','visper','AI','size','type','model'])
